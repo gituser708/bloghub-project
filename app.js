e848270e4695e28ba5f7cac740ce3b9d597a2d85
gituser708 <donmania999@gmail.com>
2024-06-16 20:10:10 +0530

    Add debugging for views path and list files

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 require("./CONFIG/db");
 const express = require ("express");
+const path = require('path');
+const fs = require('fs');  // Import the fs module
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const methodOverride = require("method-override");
@@ -12,68 +14,86 @@ const Post = require("./MODELS/post/Post");
 const { truncatePost } = require("./UTILS/helpers");
 
 
+
 const app = express();
 
-//* helpers
-app.locals.truncatePost = truncatePost;
+// Helpers
+app.locals.truncatePost = truncatePost;  // Assuming truncatePost is defined somewhere
 
-//* middlewares
- app.use(express.json());
- app.use(express.urlencoded({extended: true}));
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
- //* method override
- app.use(methodOverride("_method"));
+// Method override
+app.use(methodOverride("_method"));
 
- //* configure ejs
- app.set("view engine", "ejs");
- //* server static files
- app.use(express.static(__dirname, + "/PUBLIC"));
+// Set the views directory
+const viewsPath = path.join(__dirname, 'views');
+console.log('Views Path:', viewsPath);  // Debugging line to check the path
+app.set('views', viewsPath);
 
- //* use session
- app.use(
-     session({
-          secret: process.env.SESSION_KEY,
-          resave: false,
-          saveUninitialized: true,
-          store: new MongoStore({
-               mongoUrl: process.env.MONGODB_URL,
-               ttl: 24 * 60 * 60, //1 day
-          }), 
-     }),
- );
-
- //* save the login user into locals
- app.use((req, res, next) => {
-     if(req.session.userAuth) {
-          res.locals.userAuth = req.session.userAuth;
-     } else {
-          res.locals.userAuth = null;
-     };
-     next();
- });
-
- //* render home
-app.get('/', async (req, res) => {
-     try {
-      const posts = await Post.find().populate("user");
-     res.render('index.ejs', {posts});
-     } catch(error) {
-          res.render("index.ejs", {error: error.message});
-     };
+// Debugging: List files in the views directory
+fs.readdir(viewsPath, (err, files) => {
+  if (err) {
+    console.error('Error reading views directory:', err);
+  } else {
+    console.log('Files in views directory:', files);
+  }
 });
 
-//* users route
-app.use("/api/v1/users", userRoutes);
+// Configure EJS
+app.set('view engine', 'ejs');
 
-//* post routes
-app.use("/api/v1/posts", postRoutes);
+// Serve static files
+app.use(express.static(path.join(__dirname, 'PUBLIC')));
 
-//* comments routes
-app.use("/api/v1/comments", commentRoutes);
+// Use session
+app.use(
+  session({
+    secret: process.env.SESSION_KEY,
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({
+      mongoUrl: process.env.MONGODB_URL,
+      ttl: 24 * 60 * 60, // 1 day
+    }), 
+  })
+);
 
-//* error handler middlewares
+// Save the login user into locals
+app.use((req, res, next) => {
+  if (req.session.userAuth) {
+    res.locals.userAuth = req.session.userAuth;
+  } else {
+    res.locals.userAuth = null;
+  }
+  next();
+});
+
+// Render home
+app.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find().populate("user");
+    res.render('index', { posts });
+  } catch (error) {
+    res.render('index', { error: error.message });
+  }
+});
+
+// Users route
+app.use('/api/v1/users', userRoutes);
+
+// Post routes
+app.use('/api/v1/posts', postRoutes);
+
+// Comments routes
+app.use('/api/v1/comments', commentRoutes);
+
+// Error handler middlewares
 app.use(globalErrHandler);
-//* start server
+
+// Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, 
-     console.log(`Server is running on ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
